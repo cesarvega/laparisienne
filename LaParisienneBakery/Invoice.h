7a59d1a6ef1cb4a128729f8cefba064335daa8a8
cynthia besada <cbesa002@fiu.edu>
2013-05-09 20:11:50 -0400

    Data Model classes created.

---
+++ LaParisienneBakery/Invoice.h
@@ -0,0 +1,22 @@
+//
+//  Invoice.h
+//  LaParisienneBakery
+//
+//  Created by cynthia besada on 5/9/13.
+//  Copyright (c) 2013 cynthia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Invoice : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * custID;
+@property (nonatomic, retain) NSString * department;
+@property (nonatomic, retain) NSDate * docDate;
+@property (nonatomic, retain) NSString * docNum;
+@property (nonatomic, retain) NSDecimalNumber * docTotal;
+@property (nonatomic, retain) NSNumber * invoiceID;
+
+@end
