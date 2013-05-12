aaca26aea96b9527095be1f01e12645dad47806b
Cesar Vega <cesarvega.col@gmail.com>
2013-05-12 00:55:52 -0400

    find cyntias

---
+++ ManageClientsViewController.m
@@ -0,0 +1,38 @@
+//
+//  ManageClientsViewController.m
+//  LaParisienneBakery
+//
+//  Created by Cesar Vega on 5/11/13.
+//  Copyright (c) 2013 cynthia. All rights reserved.
+//
+
+#import "ManageClientsViewController.h"
+
+@interface ManageClientsViewController ()
+
+@end
+
+@implementation ManageClientsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
