f08ad603f6fa35b90961f4e97f2d580433f176ae
Cesar Vega <cesarvega.col@gmail.com>
2013-05-11 20:51:28 -0400

    HomeScreen

---
+++ LogInViewController.m
@@ -0,0 +1,35 @@
+//
+//  LogInViewController.m
+//  LaParisienneBakery
+//
+//  Created by Cesar Vega on 5/11/13.
+//  Copyright (c) 2013 cynthia. All rights reserved.
+//
+
+#import "LogInViewController.h"
+
+@interface LogInViewController ()
+
+@end
+
+@implementation LogInViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
