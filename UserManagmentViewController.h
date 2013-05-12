a405d321c7c427137c5403847f800b542853d437
cynthia besada <cbesa002@fiu.edu>
2013-05-12 01:09:29 -0400

    user management

---
+++ UserManagmentViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserManagmentViewController.h
+//  LaParisienneBakery
+//
+//  Created by cynthia besada on 5/12/13.
+//  Copyright (c) 2013 cynthia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserManagmentViewController : UIViewController
+
+@end
