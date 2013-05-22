dc98db9fca61a2f4e573564a45c47743453f9c14
Cesar Vega <cesarvega.col@gmail.com>
2013-05-21 22:04:14 -0400

    commit

---
+++ dummassViewController.h
@@ -0,0 +1,13 @@
+//
+//  dummassViewController.h
+//  LaParisienneBakery
+//
+//  Created by Cesar Vega on 5/21/13.
+//  Copyright (c) 2013 cynthia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface dummassViewController : UIViewController
+
+@end
---
+++ dummassViewController.m
@@ -0,0 +1,38 @@
+//
+//  dummassViewController.m
+//  LaParisienneBakery
+//
+//  Created by Cesar Vega on 5/21/13.
+//  Copyright (c) 2013 cynthia. All rights reserved.
+//
+
+#import "dummassViewController.h"
+
+@interface dummassViewController ()
+
+@end
+
+@implementation dummassViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
