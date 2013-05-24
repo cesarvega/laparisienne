65c2ddda8347ee58046197b45a53b19b1bbc5c10
Cesar Vega <cesarvega.col@gmail.com>
2013-05-24 17:48:06 -0400

    commit

--- ManageInvoicesViewController.h
+++ ManageInvoicesViewController.h
@@ -8,6 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ManageInvoicesViewController : UIViewController
+@interface ManageInvoicesViewController : UIViewController{
+    UIButton *printButton;
+    UIPrintInteractionController *printController;
+}
 
 @end
